21f4684b3ccded7f806c7ab289718757729bbf18
CharlotteH86 <70633528+CharlotteH86@users.noreply.github.com>
2020-10-22 11:14:43 +0200

    Added comments

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
+//Varibel med länkar till slumpade bilder.
 const animalUrls = {
   cat: "https://aws.random.cat/meow",
   dog: "https://random.dog/woof.json",
   fox: "https://randomfox.ca/floof/",
 };
 
+//Variabler innehållande koppling till knappar, bilder osv.
 const spinnerImgSrc = "./images/spinner.gif";
 const fetchButtonEl = document.querySelector(".fetch-animal-button");
 const animalSelectEl = document.querySelector(".animal-select");
 const animalImageEl = document.querySelector(".animal-image img");
 const favoriteImagesEl = document.querySelector(".favorite-images");
 
+//addEventListener som aktiveras vid klick på knappen "fetch image".
 fetchButtonEl.addEventListener("click", () => {
   animalImageEl.src = spinnerImgSrc;
 
   const animalType = animalSelectEl.value;
   const animalUrl = animalUrls[animalType];
-
+  //fetch hämtar länkarna från cat, dog eller fox beroende på vilket djur som är valt under .animal-select.
   fetch(animalUrl)
     .then((response) => response.json())
     .then((animalData) => {
@@ -31,13 +34,15 @@ fetchButtonEl.addEventListener("click", () => {
     });
 });
 
-animalImageEl.addEventListener("click", e => {
+//Vid klick på bild läggs nuvarande bild till i favorit delen av sidan. Under klassen favorites-images.
+animalImageEl.addEventListener("click", (e) => {
   displayFavoriteImage(e.target.src);
 });
 
+//Funktionen för att lägga till en ny bild till favorite-images.
 function displayFavoriteImage(imageSrc) {
   const newFavoriteEl = document.createElement("img");
   newFavoriteEl.src = imageSrc;
   favoriteImagesEl.prepend(newFavoriteEl);
-  favoriteImagesEl.scrollTo(0,0);
+  favoriteImagesEl.scrollTo(0, 0);
 }
